d7958962018178a7ee03463732322221c0516f16
Nathan Fenner <nfenneremail@gmail.com>
2017-12-22 13:07:50 -0500

    improve type-safety with common types between server and client (#5)

---
+++ common.ts
@@ -0,0 +1,66 @@
+export type Area = "beach" | "ocean" | "forest";
+
+export type ViewState = {
+    state: "draw",
+    deckSize: number,
+} | {
+    state: "play",
+    hand: Hand,
+    deckSize: number,
+};
+
+export type Move = {move: "draw"} | {move: "end"} | {move: "buy3", using: number[]};
+
+
+export type RewardCard = {
+    rewardType:     Area | "starter";
+    rarityType:     "rare" | "common";
+    shipParts: number; // use towards end game
+    provisions: number; // use towards end game
+    energyStores: number; // shuffles into deck, use to buy future challenge cards 
+};
+
+export type ChallengeCard = {
+    challengeType: Area;
+    challengeText: string;
+    challengeOdds: number[]; //TODO
+    challengeOutcomes: Outcome[];
+};
+
+export type Outcome = {
+    outcomeType: "reward" | "bonus" | "discard" | "nothing";
+}
+
+export type BonusCard = {
+    bonusType: string;
+    bonusText: string;
+}
+
+export class Deck<T> {
+    private cards: T[];
+    constructor() {
+        this.cards = [];
+    }
+    draw(): T {
+        if (this.cards.length == 0) {
+            throw "cannot draw from empty deck";
+        }
+        let index = Math.random() * this.cards.length | 0;
+        let out = this.cards[index];
+        this.cards[index] = this.cards[this.cards.length-1];
+        this.cards.pop();
+        return out;
+    }
+    size(): number {
+        return this.cards.length;
+    }
+    insert(card: T) {
+        this.cards.push(card);
+    }
+}
+
+export type Hand = {
+    cards: RewardCard[]
+};
+
+
